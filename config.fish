a614e894814e4c8f8354d87dee6379c4f4557dbb
ddjfreedom <ddj1990@gmail.com>
2011-01-26 21:59:53 +0800

    deleted function rchmod

--- config.fish
+++ config.fish
@@ -10,37 +10,3 @@ end
 function wall
   python ~/Proxy/wallproxy/local/proxy.py >/dev/null ^&1 &
 end
-
-# change file modes recursively
-function rchmod
-  set -l msg "change file modes recursively
-usage: rchmod [directory-mode|- file-mode|-]|[-h]"
-  if [ -z "$argv" ]
-    echo $msg
-  else
-    if [ $argv[1] = "-h" ]
-      echo $msg
-    else
-      if [ (count $argv) -lt 2 ]
-        echo $msg
-      else 
-        for file in *
-          if [ -d $file ]
-            cd $file
-            rchmod $argv
-            cd ..
-            if [ $argv[1] != "-" ]
-              chmod $argv[1] $file
-            end
-          else
-            if [ -f $file ] 
-              if [ $argv[2] != "-" ]
-                chmod $argv[2] $file
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-end
